2689181794a16966c547d90010e71baec08dd1e6
Masood ul Rehman <masooddulrehman@gmail.com>
2023-01-08 20:48:22 +0500

    created goal api

---
+++ backend/models/goalModel.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const goalSchema=mongoose.Schema({
+    text:{
+        type:String,
+        required:[true,'Please add a value']
+    }
+},{
+    timestamps:true
+})
+module.exports=mongoose.model('goal',goalSchema)
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express =require('express')
+const dotenv=require('dotenv').config()
+const {errorHandler}=require('./middleware/erroemiddleware')
+const connectDB=require('./config/db')
+
+
+connectDB()
+const app=express()
+
+const port=process.env.port
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+app.use(errorHandler)
+app.listen(port,()=>console.log('started'))
