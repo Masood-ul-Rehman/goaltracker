96dc0a4d030782eda332611f84446fbef36d4dd7
Masood ul Rehman <masooddulrehman@gmail.com>
2023-01-09 16:40:47 +0500

    added Jwt auth funtionality and frontend

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,15 @@
-const express=require('express')
+const express = require("express");
 
-const [getGoals, setGoal,updateGoal,deleteGoal]= require('../controlers/goalControler')
+const [
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+] = require("../controlers/goalControler");
+const { protect } = require("../middleware/authmiddleware");
 
-const router=express.Router();
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+const router = express.Router();
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
-module.exports=router;
+module.exports = router;
